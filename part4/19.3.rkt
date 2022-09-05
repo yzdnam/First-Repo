8030d1498caeb66ce89557251895d04b5eb38278
yzdnam <cmandzy@protonmail.com>
2022-09-04 22:40:07 -0500

    Completed 19.3, 19.5 in progress

--- part4/19.3.rkt
+++ part4/19.3.rkt
@@ -49,9 +49,142 @@
                     (cond
                       [(empty? sl) 1]
                       [else
-                       (+ (depth (first sl)) (depth-sl (rest sl)))])))
-            (depth-sl sexpr))]))
+                       (cond
+                         [(atom? sl) (+ 1 (depth sl))]
+                         [else (+ 1 (depth-sl (first sl)))]
+                         )])))
+            (cond
+              [(>= (depth-sl (first sexpr))(depth-sl (rest sexpr))) (depth-sl (first sexpr))]
+              [else (depth (rest sexpr))]))]))
 
 (check-expect (depth 'world) 1)
 (check-expect (depth '(world hello)) 2)
 (check-expect (depth '(((world) hello) hello)) 4)
+(check-expect (depth (list (list (list 'world) 'hello) 'hello)) 4)
+(check-expect (depth (list 'world (list (list (list (list 'hello)) 'world)))) 6)
+
+; EX 319
+; S-expr Symbol Symbol -> S-expr
+; replaces all of the first given symbol with the second given symbol in the given S-expr
+(define (substitute sexpr old new)
+  (local (
+          (define (sub-atom atom)
+            (cond
+              [(equal? atom old) new]
+              [else atom]))
+
+          (define (sub-sl sl)
+            (cond
+              [(empty? sl) '()]
+              [else (cond
+                      [(atom? (first sl)) (cons (sub-atom (first sl)) (sub-sl (rest sl)))]
+                      [else (cons (sub-sl (first sl)) (sub-sl (rest sl)))])])))
+          
+  (cond
+    [(atom? sexpr) (sub-atom sexpr)]
+    [else (sub-sl sexpr)])))
+
+
+(check-expect (substitute 'world 'world 'hello) 'hello)
+(check-expect (substitute '(world hello) 'world 'hello) '(hello hello))
+(check-expect (substitute '(((world) hello) hello) 'hello 'fart) '(((world) fart) fart))
+
+; EX 320
+; GIVEN DATA DEFINITION
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+	
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol 
+
+; ALTERNATIVE DATA DEFINTION
+; expand the first clause of the first data definition into the 3 clauses of Atom
+; and use the List-of Abstraction in the second data definition
+
+; An S-expr is one of: 
+; – Number
+; – String
+; – Symbol
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – [List-of S-expr]
+
+; S-expr Symbol -> N 
+; counts all occurrences of sy in sexp
+(define (count-alt sexpr sy)
+  (cond
+    [(number? sexpr) 0]
+    [(string? sexpr) 0]
+    [(symbol? sexpr) (if (symbol=? sexpr sy) 1 0)]
+    [(list? sexpr) (local (
+                           (define (count-sl-alt sexpr)
+                             (cond
+                               [(empty? sexpr) 0]
+                               [else
+                                 (+ (count-alt (first sexpr) sy) (count-sl-alt (rest sexpr)))])))
+                     (count-sl-alt sexpr))]))
+
+(check-expect (count-alt 'world 'hello) 0)
+(check-expect (count-alt '(world hello) 'hello) 1)
+(check-expect (count-alt '(((world) hello) hello) 'hello) 2)
+
+; 2ND ALT DATA DEFINITION
+
+; An S-expr is one of:
+; - Number
+; - String
+; - Symbol
+; - '()
+; - [List-of S-expr]
+
+(define (count-alt2 sexpr sy)
+  (cond
+    [(number? sexpr) 0]
+    [(string? sexpr) 0]
+    [(symbol? sexpr) (if (symbol=? sexpr sy) 1 0)]
+    [(empty? sexpr) 0]
+    [(list? sexpr) (+ (count-alt2 (first sexpr) sy) (count-alt2 (rest sexpr) sy))]))
+
+(check-expect (count-alt2 'world 'hello) 0)
+(check-expect (count-alt2 '(world hello) 'hello) 1)
+(check-expect (count-alt2 '(((world) hello) hello) 'hello) 2)
+
+; EX 321
+; GIVEN DATA DEFINITION
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+	
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol
+
+; Abstracted data definitions for S-expr and SL
+; They're already abstracted
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+	
+; An Atom is one of: 
+; – X
+; – Y
+; – Z
+; - ...
---
+++ part4/19.5.rkt
@@ -0,0 +1,111 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |19.5|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct no-info [])
+(define NONE (make-no-info))
+ 
+(define-struct node [ssn name left right])
+; A BT (short for BinaryTree) is one of:
+; – NONE
+; – (make-node Number Symbol BT BT)
+
+; EX 322
+; BT Number -> Boolean
+; Determines whether a given number occurs in a BT
+(define (contains-bt? abt n)
+  (cond
+    [(no-info? abt) #false]
+    [else
+     (cond
+       [(equal? (node-ssn abt) n) #true]
+       [else (or (contains-bt? (node-left abt) n)
+                 (contains-bt? (node-right abt) n))])]))
+
+(define bt1 (make-node
+  15
+  'd
+  NONE
+  (make-node
+    24 'i NONE NONE)))
+
+(define bt2 (make-node
+  15
+  'd
+  (make-node
+    87 'h NONE NONE)
+  NONE))
+
+(check-expect (contains-bt? bt1 24) #true)
+(check-expect (contains-bt? bt2 71) #false)
+
+; EX 323
+; BT Number -> Symbol
+; Returns the name of the BT with the given Number as its ssn
+; #false if no BTs in the given BT have the given Number as an ssn
+(define (search-bt abt n)
+  (cond
+    [(no-info? abt) #false]
+    [else
+     (cond
+       [(equal? (node-ssn abt) n) (node-name abt)]
+       [else (if (boolean? (search-bt (node-left abt) n)) (search-bt (node-right abt) n)
+                 (search-bt (node-left abt) n))])]))
+
+
+(check-expect (search-bt bt1 24) 'i)
+(check-expect (search-bt bt2 71) #false)
+
+; EX 324
+; BT -> [List-of Number]
+; returns the sequence of all ssn numbers from the BT as they show up
+; from left to right when looking at the tree diagram
+(define (inorder abt)
+  (cond
+    [(no-info? abt) '()]
+    [else (append (inorder (node-left abt)) (list (node-ssn abt)) (inorder (node-right abt)))]))
+
+(check-expect (inorder bt1) (list 15 24))
+(check-expect (inorder bt2) (list 87 15))
+(check-expect (inorder bt3) (list 15 12 24))
+
+(define bt3 (make-node
+  15
+  'd
+  NONE
+  (make-node
+    24 'i (make-node 12 's NONE NONE) NONE)))
+
+; EX 325
+; BT Number -> Symbol
+; Returns the name of the BT with the given Number as its ssn
+; NONE if no BTs in the given BT have the given Number as an ssn
+; uses the BST invariant to increase efficiency
+(define (search-bst abt n)
+  (cond
+    [(no-info? abt) 'NONE]
+    [else
+     (cond
+       [(equal? (node-ssn abt) n) (node-name abt)]
+       [else (if (> (node-ssn abt) n) (search-bst (node-left abt) n)
+                 (search-bst (node-right abt) n))])]))
+
+(check-expect (search-bst bt1 24) 'i)
+(check-expect (search-bst bt2 71) 'NONE)
+
+; EX 326
+; BST Number Symbol -> BST
+; inserts a node with the given N and S into the given BST
+(define (create-bst abst n sym)
+  (cond
+    [(no-info? abst) (make-node n sym NONE NONE)]
+    [else (if (> (node-ssn abst) n)
+              (make-node (node-ssn abst) (node-name abst) (create-bst (node-left abst) n sym) (node-right abst))
+              (make-node (node-ssn abst) (node-name abst) (node-left abst) (create-bst (node-right abst) n sym)))]))
+
+(define EXAMPLE-BST (make-node 63 'e (make-node 29 'd (make-node 15 'b (make-node 10 'a NONE NONE) (make-node 24 'c NONE NONE)) NONE)
+                               (make-node 89 'g (make-node 77 'f NONE NONE) (make-node 95 'h NONE (make-node 99 'i NONE NONE)))))
+
+(check-expect (create-bst EXAMPLE-BST 12 'x)
+              (make-node 63 'e (make-node 29 'd (make-node 15 'b (make-node 10 'a NONE (make-node 12 'x NONE NONE)) (make-node 24 'c NONE NONE)) NONE)
+                               (make-node 89 'g (make-node 77 'f NONE NONE) (make-node 95 'h NONE (make-node 99 'i NONE NONE)))))
+
