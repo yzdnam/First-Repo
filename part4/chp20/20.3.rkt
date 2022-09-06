ac4d06ce642c38af5c8739da46045195b2ee1528
yzdnam <cmandzy@protonmail.com>
2022-09-05 22:02:46 -0500

    Section 20.3 in progress

--- part4/chp20/20.3.rkt
+++ part4/chp20/20.3.rkt
@@ -3,21 +3,26 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |20.3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require htdp/dir)
 
-(define H (create-dir "/home/acerlaptop/documents/2_school"))
+;(define Hlp (create-dir "/home/acerlaptop/documents/2_school"))
+(define Hdt (create-dir "/home/admin/documents/2_school/cs/htc/first-repo"))
 
-; A File.v3 is a structure: 
+; A File is a structure: 
 ;   (make-file String N String)
 
-; A Dir.v3 is a structure: 
+; A Dir is a structure: 
 ;   (make-dir.v3 String Dir* File*)
 
 ; A Dir* is one of: 
 ; – '()
-; – [List-of Dir.v3]
+; – [List-of Dir]
  
 ; A File* is one of: 
 ; – '()
-; – [List-of File.v3]
+; – [List-of File]
+
+;(define-struct file [name size content])
+
+;(define-struct dir [name dirs files])
 
 ; EX 338
 (define (how-many direct)
@@ -25,7 +30,38 @@
      (length (dir-files direct))))
 
 ; EX 339
-; Dir.v3 string -> Boolean
+; Dir string -> Boolean
 ; determines whether the given string is the name of a file in the given directory tree or not
 (define (find? direct fname)
-  
+  (or (find-in-dir* (dir-dirs direct) fname)
+      (find-in-file* (dir-files direct) fname))) 
+
+(define (find-in-dir* lod fname)
+  (cond
+    [(empty? lod) #false]
+    [else (or (find? (first lod) fname)
+              (find-in-dir* (rest lod) fname))]))
+
+(define (find-in-file* lof fname)
+  (cond
+    [(empty? lof) #false]
+    [else (if (equal? (file-name (first lof)) fname)
+              #true
+              (find-in-file* (rest lof) fname))]))
+
+(check-expect (find? Hdt "20.2.rkt") #true)
+(check-expect (find? Hdt "fart") #false)
+
+; EX 340
+; Dir -> [List-of String]
+; lists the names of all files and directories in a given Dir
+(define (ls direct)
+  (append (ls-dirs (dir-dirs direct)) (ls-files (dir-files direct))))
+
+(define (ls-dirs lod)
+  (cond
+    ([empty? lod] '())
+    (else (cons (dir-name (first lod)) (ls-dirs (rest lod))))))
+
+(define (ls-files lof)
+  (foldl cons '() (map file-name lof)))
