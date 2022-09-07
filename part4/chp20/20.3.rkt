ec67305aa6c047e240b5b441a1c47874b88bf41a
yzdnam <cmandzy@protonmail.com>
2022-09-06 20:25:18 -0500

    Section 20.3 in progress

--- part4/chp20/20.3.rkt
+++ part4/chp20/20.3.rkt
@@ -3,8 +3,8 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |20.3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require htdp/dir)
 
-(define Hlp (create-dir "/home/acerlaptop/documents/2_school"))
-;(define Hdt (create-dir "/home/admin/documents/2_school/cs/htc/first-repo"))
+;(define Hlp (create-dir "/home/acerlaptop/documents/2_school"))
+(define Hdt (create-dir "/home/admin/documents/2_school/cs/lab"))
 
 ; A File is a structure: 
 ;   (make-file String N String)
@@ -98,3 +98,69 @@
 ; interpretation directions into a directory tree
 
 ; EX 342
+; Dir string -> [List-of String]
+; if (find? dir filename) is #true, find produces a path to a file with name filename
+; otherwise it produces #false
+(define (find direct fname)
+  (cond
+    [(find? direct fname) (cons (dir-name direct) (if (find-in-file* (dir-files direct) fname) (path-to-file (dir-files direct) fname)
+                                                   (search-dir* (dir-dirs direct) fname)))]
+    [else #false]))
+
+; [List-of Dir] string -> [List-of String]
+(define (search-dir* lod fname)
+  (cond
+    [(empty? lod) '()]
+    [else (if (find? (first lod) fname) (find (first lod) fname)
+              (search-dir* (rest lod) fname))]))
+
+(define (path-to-file lof fname)
+  (cond
+    [(empty? lof) '()]
+    [else (if (equal? (file-name (first lof)) fname) (list fname)
+              (path-to-file (rest lof) fname))]))
+
+; Dir string -> [List-of [List-of String]]
+; produces the list of all paths that lead to f in d
+(define (find-all direct fname)
+  (cond
+    [(and (not (empty? (path-to-file (dir-files direct) fname)))
+          (not (empty? (find-all-dirs (dir-dirs direct) fname)))) (cons (cons (dir-name direct) (path-to-file (dir-files direct) fname))
+                                                                      (map (lambda (x) (cons (dir-name direct) x)) (find-all-dirs (dir-dirs direct) fname)))]
+    
+    [(and (not (empty? (path-to-file (dir-files direct) fname)))
+          (empty? (find-all-dirs (dir-dirs direct) fname))) (cons (dir-name direct) (path-to-file (dir-files direct) fname))]
+    
+    [(and (empty? (path-to-file (dir-files direct) fname))
+          (not (empty? (find-all-dirs (dir-dirs direct) fname)))) (map (lambda (x) (cons (dir-name direct) x)) (find-all-dirs (dir-dirs direct) fname))]
+    
+    [else #false]))
+
+(define (find-all-dirs lod fname)
+  (cond
+    [(empty? lod) '()]
+    [(find? (first lod) fname) (cons (find (first lod) fname) (find-all-dirs (rest lod) fname))]
+    [else (find-all-dirs (rest lod) fname)]))
+
+(check-expect (find EXAMPLE-TREE "read!") (list "TS" "read!"))
+(check-expect (find EXAMPLE-TREE "hang") (list "TS" "Libs" "Code" "hang"))
+(check-expect (find-all EXAMPLE-TREE "read!") (list (list "TS" "read!") (list "TS" "Libs" "Docs" "read!")))
+
+; EX 343
+; Dir -> [List-of [List-of String]]
+; lists the paths to all files contained in a given Dir
+(define (ls-r direct)
+  (cons (show-all-paths (dir-dirs direct))
+        (start-paths direct (dir-files direct))))
+
+(define (show-all-paths lod)
+  (cond
+    [(empty? lod) '()]
+    [else (append (cons (dir-name (first lod)) (ls-r (first lod))) (show-all-paths (rest lod)))]))
+
+(define (start-paths direct lof)
+  (cond
+    [(empty? lof) '()]
+    [else (cons (file-name (first lof)) (start-paths direct (rest lof)))]))
+  
+(ls-r Hdt)
