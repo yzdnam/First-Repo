cf20483f18f85735ba1e1b336d19fa88f4cbf47e
yzdnam <cmandzy@protonmail.com>
2022-11-05 10:58:28 -0500

    Section 1.2.6 in progress.

--- sicp/1.2.6.rkt
+++ sicp/1.2.6.rkt
@@ -36,7 +36,25 @@
 ; EX 1.22 - write a procedure, "search-for-primes", that checks the primality of consecutive odd integers in a specified range using "timed-prime-test".
 ; test various inputs with the procedure and determine whether the results are compatible with the notion that programs on this machine run in time proportional
 ; to the number of steps required for the computation
-(define (search-for-primes >than0 how-many0)
+(define (checked-prime-test n check-val)
+  (cond
+    [(= n check-val) #f]
+    [else (timed-prime-test n)]))
+(define (search-for-primes >than <than)
+  (define check# (inc >than))
+  (cond
+    [(not (even? >than)) (search-for-primes (inc >than) <than)]
+    [(checked-prime-test check# <than) (search-for-primes (+ >than 2) <than)]
+    [else (search-for-primes (+ >than 2) <than)]))
+  
+(define (search-for-primes.1 >than0 <than0 how-many0)
   (define (search-for-primes/a >than how-many lop)
+    (define check# (inc >than))
     (cond
-      []
+      [(= how-many 0) lop]
+      [(not (even? >than)) (search-for-primes/a (inc >than) how-many lop)]
+      [(prime? check#) (search-for-primes/a (+ >than 2) (dec how-many) (cons check# lop))]
+      [else (search-for-primes/a (+ >than 2) how-many lop)]))
+  (define (run-test lop)
+    (cond
+      [(eqv? lop '()
