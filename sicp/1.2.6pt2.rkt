2771eb68a842b1c88f33a63c847a54a76c49b3b8
yzdnam <cmandzy@protonmail.com>
2022-11-06 20:30:36 -0600

    Section 1.2.6 in progress.

--- sicp/1.2.6pt2.rkt
+++ sicp/1.2.6pt2.rkt
@@ -9,7 +9,15 @@
         (else (* b (fast-expt b (- n 1))))))
 
 (define (expmod base exp m)
-  (remainder (fast-expt base exp) m))
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
 
 ; EX 1.27
 ; write a procedure that demonstrates that the Carmichael numbers really do fool the Fermat test
@@ -21,6 +29,29 @@
       ((= (expmod a n n) a) (ex1.27/a (inc a)))
       (else #f)))
   (ex1.27/a 1))
+; -----
+
+(define (fermat-test n)
+(define (try-it a)
+ (= (expmod a n n) a))
+(try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+(cond ((= times 0) true)
+((fermat-test n) (fast-prime? n (- times 1)))
+(else false)))
 
 ; EX 1.28
-        
+; write a procedure that implements the Miller-Rabin Test for testing the primality of a number
+(define (m-rexpmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (m-rexpmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (m-rexpmod base (- exp 1) m))
+          m))))
+
+(define (m-r n)
