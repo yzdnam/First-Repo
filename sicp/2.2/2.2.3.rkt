efb5764d55b33c46bb1e10d9070869ea727c81e3
yzdnam <cmandzy@protonmail.com>
2022-12-04 16:44:57 -0600

    Section 2.2.3 in progress.

--- sicp/2.2/2.2.3.rkt
+++ sicp/2.2/2.2.3.rkt
@@ -2,6 +2,23 @@
 
 (define (square n) (* n n))
 
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
 (define (filter predicate sequence)
   (cond ((null? sequence) nil)
         ((predicate (car sequence))
@@ -23,9 +40,9 @@
 (define (length2.33 sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
-(map2.33 square (list 1 2 3))
-(append2.33 (list 1 2 3) (list 4 5 6))
-(length2.33 (list 1 2 3))
+;(map2.33 square (list 1 2 3))
+;(append2.33 (list 1 2 3) (list 4 5 6))
+;(length2.33 (list 1 2 3))
 
 ; EX 2.34
 ; complete the following procedure that evaluates a polynomial using Horner's rule
@@ -42,11 +59,11 @@
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
 (define test-tree (list 1 (list 2 (list 3 4))))
-(count-leaves test-tree)
+;(count-leaves test-tree)
 
 (define (count-leaves-accum x)
   (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves-accum x) 1)) x)))
-(count-leaves-accum test-tree)
+;(count-leaves-accum test-tree)
 
 ; EX 2.36
 ; accumulate-n takes as its 3rd argument a sequence of sequences, which are all assumed to have the same number of elements. It applies the designated accumulation
@@ -58,7 +75,7 @@
       (cons (accumulate op init (map (lambda (x) (car x)) seqs))
             (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
 (define test-seq-seqs '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
-(accumulate-n + 0 test-seq-seqs)
+;(accumulate-n + 0 test-seq-seqs)
 
 ; EX 2.37
 ; complete the following procedures which manipulate matrices
@@ -71,12 +88,12 @@
 
 (define test-matrix (list (list 1 -1 2) (list 0 -3 1)))
 (define test-vector (list 2 1 0))
-(matrix-*-vector test-matrix test-vector)
+;(matrix-*-vector test-matrix test-vector)
 
 (define (transpose mat)
   (accumulate-n cons '() mat))
 
-(transpose test-matrix)
+;(transpose test-matrix)
 
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
@@ -84,7 +101,150 @@
 
 (define test-matrixA (list (list 0 4 -2) (list -4 -3 0)))
 (define test-matrixB (list (list 0 1) (list 1 -1) (list 2 3)))
-(matrix-*-matrix test-matrixA test-matrixB)
+;(matrix-*-matrix test-matrixA test-matrixB)
 
 ; EX 2.38
-  
+(define (foldl op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+       (iter (op result (car rest))
+               (cdr rest))))
+  (iter initial sequence))
+
+(define (foldr op initial sequence)
+  (accumulate op initial sequence))
+; op must be commutative and associative for fold-left and accumulate (or fold-right) to produce the same results for any given
+; sequence
+
+; EX 2.39
+; complete the following definitions of reverse in terms of foldl and foldr
+(define (reverse2.391 sequence)
+  (foldr (lambda (x y) (append y (list x))) nil sequence))
+(define (reverse2.392 sequence)
+  (foldl (lambda (x y) (append (list y) x)) nil sequence))
+;(reverse2.391 (list 1 2 3))
+;(reverse2.392 (list 1 2 3))
+
+; required functions for EX 2.40 and beyond
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (flatmap
+                            (lambda (i)
+                              (map (lambda (j) (list i j))
+                                   (enumerate-interval 1 (- i 1))))
+                            (enumerate-interval 1 n)))))
+
+; EX 2.40
+; Define a procedure unique-pairs that, given an integer n, generates the sequence of pairs (i, j) with 1 <= j < i <= n and then
+; use unique pairs to simplify the definition of prime-sum-pairs given above
+(define (unique-pairs n)
+  (flatmap
+   (lambda (i)
+     (map (lambda (j) (list i j))
+          (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs2.40 n)
+  (map make-pair-sum (filter prime-sum? (unique-pairs n))))
+
+;(prime-sum-pairs 5)
+;(prime-sum-pairs2.40 5)
+
+; EX 2.41
+; Write a procedure to find all ordered triples of distinct positive integers i, j, and k less than or equal to a given integer
+; n that sum to a given integer s.
+
+; helper for 2.41
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+; true if the sum of the numbers in a given triple equal a number, n
+(define (trip-eq? trip n)
+  (define (sum li)
+    (if (null? li) 0 (+ (car li) (sum (cdr li)))))
+  (if (> (length trip) 3) (error "trip-eq?: given list not a triple") (= (sum trip) n)))
+
+(define (triple-sum n s)
+  (filter (lambda (triple) (trip-eq? triple s))
+          (flatmap
+           (lambda (pair)
+             (map (lambda (new) (cons new pair))
+                  (enumerate-interval (+ 1 (car pair)) n)))
+           (unique-pairs n))))
+
+; EX 2.42
+; for the queens function, a board is a list
+; an empty board is an empty list
+; if a queen is placed in the first column of the board from the right, a pair representing where the queen is placed
+; consisting of the following will be cons'd to the board:
+; -the number of rows from the top of the board, starting with 0, where the queen is placed will be the car of the pair
+; -the number of columns from the right edge of the board starting with 0 where the queen is placed will be the cdr of the pair
+; to illustrate, a board with queens at the following positions:
+; -first column from the right, 3 rows from the top edge of the board
+; -second column from the right, at the top row of the board
+; will be represented as such:
+; '((0 1) (2 0))
+; to summarize, a queen position will be represented by a pair in the form
+; '(row# column#)
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 0 (- board-size 1))))
+            (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+(define safe-test-board '((0 1) (2 0)))
+(define unsafe-test-board '((1 3) (2 2) (3 1) (4 0)))
+
+(define (safe? k positions) 
+  (let ((check-queen (list-ref (reverse positions) (- k 1)))
+        (check-positions (remove-i (- k 1) (reverse positions))))
+    (define (check-safe queen other-queens)
+      (if (not (null? other-queens))
+          (let ((other-queen (car other-queens))
+                (queen-y (car queen))
+                (queen-x (cadr queen)))
+            (cond
+              [(null? other-queens) #true]
+              [(or (= queen-y (car other-queen))
+                   (= queen-x (cadr other-queen))
+                   (= (+ queen-x queen-y) (+ (car other-queen) (cadr other-queen)))
+                   (= (- queen-x queen-y) (- (cadr other-queen) (car other-queen))))  #false]
+              [else (check-safe queen (cdr other-queens))]))
+      #true))
+      (check-safe check-queen check-positions)))
+
+(define (remove-i k list0)
+  (define (remove-i/a counter list0 list/a)
+    (cond
+      [(null? list0) (reverse list/a)]
+      [(= counter k) (remove-i/a (inc counter) (cdr list0) list/a)]
+      [else (remove-i/a (inc counter) (cdr list0) (cons (car list0) list/a))]))
+  (remove-i/a 0 list0 '()))
+
+(remove-i 2 (list 1 2 3 4 5))
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (list new-row (- k 1)) rest-of-queens))
---
+++ sicp/2.2/prime?.rkt
@@ -0,0 +1,204 @@
+#lang sicp
+
+; EX 1.21 - use the smallest-divisor procedure to find the smallest divisor for the following numbers:
+; 199 - 199
+; 1999 - 1999
+; 19999 - 7
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (tpt1 n)
+  (newline)
+  (display n)
+(start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+; EX 1.22 - write a procedure, "search-for-primes", that checks the primality of consecutive odd integers in a specified range using "timed-prime-test".
+; test various inputs with the procedure and determine whether the results are compatible with the notion that programs on this machine run in time proportional
+; to the number of steps required for the computation
+(define (search-for-primes >than range)
+  (define check# (inc >than))
+  (cond
+    [(<= range 2) (tpt1 check#)]
+    [(not (even? >than)) (search-for-primes (inc >than) (dec range))]
+    [else (begin (tpt1 check#) (search-for-primes (+ >than 2) (- range 2)))]))
+
+; Prime # - Time to find
+; 1009 - 5 ms
+; 1013 - 4 ms
+; 1019 - 4 ms
+
+; 10007 - 7 ms
+; 10009 - 9 ms
+; 10037 - 8 ms
+
+; 100003 - 30 ms
+; 100019 - 28 ms
+; 100043 - 28 ms
+
+; 1000003 - 84 ms
+; 1000033 - 85 ms
+; 1000037 - 85 ms
+
+; The above results are not compatible with the notion that programs on this machine run in time proportional to the number of steps required for the computation
+
+; EX 1.23
+; define the procedure "next" that returns 3 if its input is equal to 2 and otherwise returns its input plus 2
+(define (next n)
+  (cond
+    [(= n 2) 3]
+    [else (+ n 2)]))
+; modify the "smallest-divisor" procedure to use (next test-divisor) instead of (+ test-divisor 1)
+(define (smallest-divisor.v2 n) (find-divisor.v2 n 2))
+
+(define (find-divisor.v2 n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor.v2 n (next test-divisor)))))
+; use "timed-prime-test" with this modified version of "smallest-divisor" on the 12 primes found in EX 1.22
+(define (prime?.v2 n)
+  (= n (smallest-divisor.v2 n)))
+
+(define (tpt2 n)
+  (newline)
+  (display n)
+(start-prime-test.v2 n (runtime)))
+(define (start-prime-test.v2 n start-time)
+  (if (prime?.v2 n)
+      (report-prime (- (runtime) start-time))))
+
+; Prime # - Time to find with "timed-prime-test.v2"
+; 1009 - 5 ms
+; 1013 - 4 ms
+; 1019 - 4 ms
+
+; 10007 - 7 ms
+; 10009 - 9 ms
+; 10037 - 8 ms
+
+; 100003 - 30 ms
+; 100019 - 28 ms
+; 100043 - 28 ms
+
+; 1000003 - 84 ms
+; 1000033 - 85 ms
+; 1000037 - 85 ms
+
+; the 2nd versions of timed-prime-test does not run twice as fast as the first version even though the number of test steps is halved
+; This can be explained by the fact that the "next" procedure causes additional primitive operations to be performed per test step. more data would need to be
+; collected to determine a ratio of the speeds of the two algorithms. 
+
+; v3 of timed-prime-test was an attempt to test the given prime number 1000 times and display the average time for the process to run to completion.
+; this attempt failed. "compile-times" returns a list of very similar numbers except for the last one on the list. The last number on the list is the first
+; time "start-prime-test" is called. My hypothesis is that there is a yet to be investigated mechanism in this machine that optimizes the execution of
+; a procedure that is called multiple times during the execution of another procedure.
+(define (tpt3 n)
+  (newline)
+  (display n)
+(define (compile-times counter lot n)
+  (cond
+    [(= counter 0) lot]
+    [else (compile-times (dec counter) (cons (start-prime-test.v3 n (runtime)) lot) n)]))
+(report-prime (list-avg (compile-times 1000 '()))))
+(define (start-prime-test.v3 n start-time)
+  (if (prime?.v2 n)
+      (- (runtime) start-time)))
+(define (list-avg loi)
+  (define (list-avg/a loi count)
+    (cond
+      [(eqv? (cdr loi) '()) (/ (car loi) count)]
+      [else (list-avg/a (cons (+ (car loi) (car (cdr loi))) (cdr (cdr loi))) (inc count))]))
+  (list-avg/a loi 1))
+(define (compile-times counter lot n)
+  (cond
+    [(= counter 0) lot]
+    [else (compile-times (dec counter) (cons (start-prime-test.v3 n (runtime)) lot) n)]))
+
+; EX 1.24
+; conduct the same experiment as in EX 1.23 except with "fast-prime" as the prime number test
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+(define (try-it a)
+ (= (expmod a n n) a))
+(try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+(cond ((= times 0) true)
+((fermat-test n) (fast-prime? n (- times 1)))
+(else false)))
+
+(define (tpt4 n)
+  (newline)
+  (display n)
+(start-prime-test.v4 n (runtime)))
+(define (start-prime-test.v4 n start-time)
+  (if (fast-prime? n 10)
+      (report-prime (- (runtime) start-time))))
+
+; Although the Fermat test has θ(log n) growth, testing primes near 1,000,000 does not take (log 1000) times as long as testing primes near 1000.
+; my hypothesis is that the processor is powerful enough to complete all operations in one cycle hence the times measured are those of a processor cycle
+
+; EX 1.25
+; alternative expmod?
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (expmod.v2 base exp m)
+  (remainder (fast-expt base exp) m))
+
+; They return the same results for the tested inputs.
+
+; EX 1.26
+; explain why expmod.v3 is a θ(n) process instead of a θ(log n) process like expmod and expmod.v2
+(define (expmod.v3 base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod.v3 base (/ exp 2) m)
+                       (expmod.v3 base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base
+                       (expmod.v3 base (- exp 1) m))
+                    m))))
+; expmod and expmod.v2 will call "square" θ(log n) times
+; expmod.v3 calls itself twice during its execution therefore it will call * on the order of 2^(log-base2 n) times which is equivalent to n.
+; the solutions page explains it by showing that doubling the size of the input of an expmod operation that initially took n steps will result in an operation
+; taking n+1 steps. this shows that its order of growth is logarithmic.
+; on the other hand, when the size of the input for an expmod.v3 operation taking n steps is doubled, the number of steps is doubled as well meaning the its order
+; of growth is linear
+(define (fe n)
+  (+ n (* 3 (floor (/ (log n) (log 2))))))
