30d1e40169a933674e8d9ed2eac04f329b6684c7
yzdnam <cmandzy@protonmail.com>
2023-01-05 13:56:05 -0600

    completed 2.4.3, 2.5.1 and 2.5.2.

--- sicp/2.4/2.4.3pt3.rkt
+++ sicp/2.4/2.4.3pt3.rkt
@@ -41,3 +41,34 @@
 ; put operations which will interface the utilities with the rest of the system. the personnel file, along with each record
 ; contained in it, must also be affixed with a tag identifying its location/type so that the central utilities can call the
 ; appropriate division-specific utilities when called on the new file and records.
+
+; EX 2.75
+; implement make-from-mag-ang in message-passing style.
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real part) (* x (cos y)))
+          ((eq? op 'imag-part) (* x (sin y)))
+          ((eq? op 'magnitude) x)
+          ((eq? op 'angle) y)
+          (else (error "Uknown op: MAKE-FROM-MAG-ANG" op))))
+  dispatch)
+
+; EX 2.76
+; for each of the 3 strategies for implementing generic operations, explicit dispatch, data-directed style, and message-passing style, describe the changes that must
+; be made to a system in order to add new types or new operations.
+
+; explicit dispatch:
+; to add new types, each operation in the system would need to be modified with a new condition handling the new type of data
+; to add new operations, define a new operation with a conditional that handles each type of data in the system
+
+; data-directed style:
+; to add new types, install a package of utilities that handle the new type of data and interface the package with the rest of the system using a put procedure.
+; operations in the system will interface with the new type using the get procedure
+; to add new operations, the operation would need to be defined within each type package.
+
+; message-passing style:
+; to add new types, define a new constructor procedure consisting of a conditional that handles each operation in the system for that data-type
+; to add new operations, each type in the system would need to be modified with a new condition that dispatches the new operation
+
+; in a system where new types must often be added but new operations are not, message-passing is most appropriate.
+; in a system where new operations must often be added but new types are not, explicit dispatch would be most appropriate
